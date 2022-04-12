1354072baebe9e568e846e646309d0c68e53306e
Kurniaman <87873299+Kurniaman@users.noreply.github.com>
2022-04-12 13:45:50 +0700

    Add files via upload

---
+++ Frontend Practice/js/script.js
@@ -0,0 +1,16 @@
+// ini adalah komentar
+console.log("Hello World");
+console.log(5+3);
+console.log(2%3);
+let name ="Kurniaman";
+const secondName = "Zega";
+console.log(name +" "+secondName);
+// ` --> backtick
+// const --> agar variabel tidak di ubah-ubah
+const fixName = "Ninja Ken";
+console.log(`Halo ${fixName}`);
+let a = 3;
+if (a>2) {
+    console.log("Hai");
+}
+// && --> and , || --> 
